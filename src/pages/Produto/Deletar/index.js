f6f703a3553a5770b592cdc96600dddb160984d5
Fulano de Tal <82171494+Lucas-Carvalho1607@users.noreply.github.com>
2021-07-07 20:40:55 -0300

    implementacao deletar

--- src/pages/Produto/Deletar/index.js
+++ src/pages/Produto/Deletar/index.js
@@ -1,23 +1,35 @@
 import React, { useState } from 'react'
-import { View, TextInput, TouchableOpacity } from 'react-native'
+import { View, TextInput, TouchableOpacity, Image } from 'react-native'
+import { deletarProduto } from '../../../data/Produto/produto_db'
 
 
-const DeletarProduto = () => {
+const Deletar = () => {
     const [id, setId] = useState(null);
+    const apagarProduto = () => {
+        deletarProduto (id)
+
+    }
+    
     return (
         <View>
             <TextInput 
+            style={{ borderStyle: 'solid', borderWidth: 1, marginTop: 10 }}
             keyboardType = 'numeric'
             name = "idProduto"
             placeholder = "id do Produto"
             onChangeText = {id => setId(id)}
             value = {id}
+            
 
             />
-            <TouchableOpacity />
+            <TouchableOpacity onPress={apagarProduto}>
+                <Image style={{ height: 50, width: 50 }}
+                    source={{ uri: 'https://th.bing.com/th/id/R.d9ffba932a78f5b38a4750f6704f2334?rik=1j1aOC5RDxXuRg&riu=http%3a%2f%2fpluspng.com%2fimg-png%2ffree-png-plus-sign-plus-sign-icon-button-green-approved-check-712.png&ehk=rIpQZlg8tb3NjbJGECZCmgLGrq%2bwOuiLS80Wpq6dshE%3d&risl=&pid=ImgRaw' }} />
+
+            </TouchableOpacity>
 
 
         </View>
     )
 }
-export default DeletarProduto;
+export default Deletar;
