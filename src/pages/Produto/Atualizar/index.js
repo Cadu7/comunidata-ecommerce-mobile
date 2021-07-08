7c5c7d260849cd23d92148fec9a1ef90626abf3e
ingridbromer <ingridb@id.uff.br>
2021-07-08 16:55:40 -0300

    CSS

--- src/pages/Produto/Atualizar/index.js
+++ src/pages/Produto/Atualizar/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, TextInput, TouchableOpacity, Image } from 'react-native'
+import { View, TextInput, TouchableOpacity, Image, Text } from 'react-native'
 import { atualizarProduto, deletarProduto, listarProdutos } from '../../../data/Produto/produto_db'
 
 const Atualizar = () => {
@@ -13,37 +13,40 @@ const Atualizar = () => {
     }
 
     return (
+        <>
+        <Text style={{fontSize: 18, textAlign: 'center', marginTop: 10 }}>Atualização</Text>
         <View style={{ padding: 10 }}>
             <TextInput
                 style={{ borderStyle: 'solid', borderWidth: 1, marginTop: 10 }}
                 keyboardType='numeric'
                 name="idProduto"
-                placeholder="id do Produto"
+                placeholder="ID do Produto"
                 onChangeText={id => setId(parseInt(id))}
                 value={id}
             />
-            <TextInput style={{ borderStyle: 'solid', borderWidth: 1 }}
+            <TextInput style={{ borderStyle: 'solid', borderWidth: 1, marginTop: 10  }}
                 name='nome'
                 placeholder='Digite o nome'
                 onChangeText={nome => setNome(nome)}
                 value={nome} />
             <TextInput style={{ borderStyle: 'solid', borderWidth: 1, marginTop: 10 }}
                 name='descricao'
-                placeholder='Digite a descricao'
+                placeholder='Digite a descrição'
                 onChangeText={descricao => setDescricao(descricao)}
                 value={descricao} />
             <TextInput keyboardType='numeric'
                 style={{ borderStyle: 'solid', borderWidth: 1, marginTop: 10 }}
                 name='preco'
-                placeholder='Digite o preco'
+                placeholder='Digite o preço'
                 onChangeText={transferPreco => setPreco(parseFloat(transferPreco))}
                 value={preco} />
             <TouchableOpacity onPress={atualizar}>
-                <Image style={{ height: 50, width: 50 }}
+                <Image style={{ height: 50, width: 50, marginLeft: 5, marginTop: 10  }}
                     source={{ uri: 'https://th.bing.com/th/id/R.d9ffba932a78f5b38a4750f6704f2334?rik=1j1aOC5RDxXuRg&riu=http%3a%2f%2fpluspng.com%2fimg-png%2ffree-png-plus-sign-plus-sign-icon-button-green-approved-check-712.png&ehk=rIpQZlg8tb3NjbJGECZCmgLGrq%2bwOuiLS80Wpq6dshE%3d&risl=&pid=ImgRaw' }} />
 
             </TouchableOpacity>
         </View>
+        </>
     )
 }
 
