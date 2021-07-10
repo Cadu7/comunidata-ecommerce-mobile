24761df9cf2bae485fda62b11288307746cb7d83
Fulano de Tal <82171494+Lucas-Carvalho1607@users.noreply.github.com>
2021-07-10 13:09:50 -0300

    Página Produto

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -5,7 +5,6 @@ const styles = StyleSheet.create({
     header: {
         backgroundColor: '#1E4B75',
         flexDirection: 'row',
-        alignItems: 'center',
         justifyContent: 'center',
         padding: 12
     },
@@ -14,8 +13,19 @@ const styles = StyleSheet.create({
         fontSize: 20,
         letterSpacing: 0.5,
         color: '#FFF',
-        fontSize: 16
+        fontSize: 16,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 10
+
     },
+    nomeText: {
+        fontSize: 15,
+        color: 'white',
+        
+
+
+    }
 });
 
 export default styles;
