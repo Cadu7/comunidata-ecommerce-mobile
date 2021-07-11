ebea8cf66e02669c6d7c643a96163f3b8612a7dd
LucasFDC91 <lucas.fdc.91@gmail.com>
2021-07-11 20:48:32 -0300

    Page Checkout montada

--- src/components/DrawerCarrinho/index.js
+++ src/components/DrawerCarrinho/index.js
@@ -3,7 +3,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import TabNavigator from '../TabNavigator'
 import Passageiros from '../../pages/Passageiros';
 import Cadastro from '../../pages/Produto/Cadastro';
-import Perfil from '../../pages/Perfil'
+import Perfil from '../../pages/Perfil';
+import Checkout from '../../pages/Checkout';
 
 
 const Drawer = createDrawerNavigator();
@@ -11,10 +12,11 @@ const Drawer = createDrawerNavigator();
 const DrawerCarrinho = () => {
     return (
         <Drawer.Navigator >
-            <Drawer.Screen name='Passageiros' component={Passageiros} />
-            <Drawer.Screen name='Cadastro' component={Cadastro} />
             <Drawer.Screen name='Home' component={TabNavigator} />
+            <Drawer.Screen name='Cadastro' component={Cadastro} />
             <Drawer.Screen name='Perfil' component={Perfil} />
+            <Drawer.Screen name='Checkout' component={Checkout} />
+            <Drawer.Screen name='Passageiros' component={Passageiros} />
         </Drawer.Navigator>
     )
 }
